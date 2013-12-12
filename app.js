cb146191c3567ebe5eddab85219f05ae9feb1fc9
Ben Ellis <dragonwolf@softweyr.co.uk>
2013-12-12 10:47:49 -0500

    Updated and Moved some files

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-var bircd = require('./build/bircd.js');
+var bircd = require('./src/bircd.js');
 var ircServer = bircd.createServer();
-
-var port = process.env.PORT;
-console.log(process.env.IP);
-console.log(port);
-ircServer.listen(16555);
+ircServer.listen(6667);
